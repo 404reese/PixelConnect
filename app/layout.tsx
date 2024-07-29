961e16fa8bc0b79cbc3222da982f1e30c4ff1d8d
404reese <riddhesh.chaudhary@gmail.com>
2024-07-29 19:37:18 +0530

    dfrt456yt

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const spaceMono = Space_Mono({
   subsets: ['latin'],
@@ -20,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={cn("font-spaceMono antialiased", spaceMono.variable)}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
